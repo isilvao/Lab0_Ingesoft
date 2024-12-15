24f432d85cce9d4ba944021def6eda3282ae12a5
Ivan Silva <isilvao@unal.edu.co>
2024-12-14 23:37:07 -0500

    Cambios requeridos para despliegue en heroku

--- Lab0/settings.py
+++ Lab0/settings.py
@@ -20,14 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rrs@%%enjqrht$!sv4aot(b97ql%kmysdlzo8jj9o7=45k4ohi"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    "*",
+]
 
 # Application definition
 
