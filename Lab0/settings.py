d651946c25b048a918d4b942d1cbe0ba9f130d53
Ivan Silva <isilvao@unal.edu.co>
2024-12-14 23:52:25 -0500

    Update settings.py

--- Lab0/settings.py
+++ Lab0/settings.py
@@ -130,7 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
